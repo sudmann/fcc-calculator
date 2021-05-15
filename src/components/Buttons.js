373742a5da3443ed20abd539bb494ab5bdcbd45f
Maximilian <maximilian@sudmann.org>
2021-05-16 00:42:39 +0200

    Add button onClick actions

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,5 @@
-
 import './buttons.css';
+import { initialCalcState } from './Calculator';
 
 const actionTypes = {
   operator: "OP",
@@ -29,6 +29,96 @@ const buttons = [
 ];
 
 const Buttons = (props) => {
+
+  // To be honest, this is function is horrible
+  const assignAction = (button) => {
+    switch (button.action) {
+      case actionTypes.clear:
+        return () => {
+          props.setCalcState(initialCalcState)
+        }
+      case actionTypes.decimal:
+        return () => {
+          props.setCalcState(state => {
+            const newNumber = state.currNumber.includes('.') ? state.currNumber : state.currNumber + '.'
+            return {
+              formula: [...state.formula],
+              currNumber: newNumber,
+              currOp: undefined,
+            };
+          });
+        }
+        case actionTypes.eval:
+          return () => {
+            props.setCalcState(state => {
+              const expr = [...state.formula, state.currOp, state.currNumber].join('');
+              console.log(expr, eval(expr));
+              return {
+                formula: [],
+                currNumber: eval(expr),
+                currOp: undefined,
+              };
+            });
+          }
+          case actionTypes.operator:
+            return () => {
+              props.setCalcState(state => {
+                if (state.currOp === undefined) {
+                  return {
+                    formula: [...state.formula, state.currNumber],
+                    currNumber: '0',
+                    currOp: button.value,
+                  }
+                } if (button.value === '-') {
+                  return {
+                    formula: [...state.formula],
+                    currNumber: state.currNumber.startsWith('-')
+                      ? state.currNumber.substring(1)
+                      : '-' + state.currNumber,
+                    currOp: state.currOp,
+                  };
+                }
+                return {
+                  formula: [...state.formula],
+                  currNumber: '0',
+                  currOp: button.value,
+                };
+              });
+            }
+            default:
+              return () => {
+                props.setCalcState(state => {
+                  if(button.value === 0 && (state.currNumber.match(/-?0$/) !== null)){
+                    return {
+                      formula: [...state.formula],
+                      currNumber: state.currNumber,
+                      currOp: state.currOp,
+                    }
+                  }
+
+                  const newFormula = [...state.formula];
+                  let newNumber = state.currNumber + button.value.toString();
+                  if (state.currNumber === '0') {
+                    newNumber = button.value.toString();
+                  }
+                  if (state.currNumber === '-0') {
+                    newNumber = '-' + button.value.toString();
+                  }
+
+                  if(state.currOp !== undefined) {
+                    newFormula .push(state.currOp);
+                  }
+
+                  return {
+                    formula: newFormula,
+                    currNumber: newNumber,
+                    currOp: undefined,
+                  }
+                });
+              };
+    }
+  }
+
   return (
     <div id="buttons">
       {
@@ -36,6 +126,7 @@ const Buttons = (props) => {
           <button
             id={ button.id }
             key={ button.id }
+            onClick={ assignAction(button) }
           >
             { button.value }
           </button>
